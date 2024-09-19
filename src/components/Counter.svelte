7e9ef3b1e2d21a3ec0f95e78c3502a246b6739c2
Domenicopezzullo <pezzullodomenico712@gmail.com>
2024-09-20 01:39:31 +0200

    Base commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let startcount : number;
+    let count = startcount
+    let increment = () => {
+        count++
+    }
+  </script>
+
+
+<div class="bg-gradient-to-r from-violet-600 to-indigo-600 h-screen w-screen flex-col flex items-center justify-center">
+  <div class="bg-white/50 text-2xl font-bold font-quicksand flex flex-col items-center justify-center py-40 px-40 rounded-xl">
+    <button on:click={increment} class="border-2 border-black font-bold text-white  hover:bg-blue-700 p-5 rounded-2xl mb-[50px] bg-blue-600 transition-all duration-300 ease-in-out transform hover:scale-125">Increment</button>
+    <h1 class="text-9xl">{count}</h1> 
+  </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.svelte"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: "Quicksand"
+      }
+    },
+  },
+  plugins: [],
+}
+
